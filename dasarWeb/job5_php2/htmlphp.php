2b9190ba658eb414adc21f5d0c67882843f52d99
tasyasss <tasyasakina0703@gmail.com>
2024-09-25 14:14:27 +0700

    job5 prak8

---
+++ dasarWeb/job5_php2/htmlphp.php
@@ -0,0 +1,18 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cara 01</title>
+</head>
+<body>
+    <p>Tanggal Hari Ini: <?php echo date("d M Y")?></p>
+</body>
+</html> -->
+
+<?php
+echo '<html>';
+echo '<head><tittle>Cara 02</tittle></head>';
+echo '<body>';
+echo '<p>Tanggal Hari Ini: '.date('d M Y').'</p>';
+echo '</body>';
+echo '</html>';
+?>
