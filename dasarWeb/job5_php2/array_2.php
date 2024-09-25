85550d93ae75b26acbaa0a935b933642b37f452f
tasyasss <tasyasakina0703@gmail.com>
2024-09-25 11:02:34 +0700

    prak2

---
+++ dasarWeb/job5_php2/array_2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $Dosen = [
+        'nama' => 'Vit Zuraida',
+        'domisili' => 'Probolinggo',
+        'jenis_kelamin' => 'Perempuan'
+    ];
+
+    echo "Nama: {$Dosen ['nama']} <br>";
+    echo "Domisili: {$Dosen ['domisili']} <br>";
+    echo "Jenis Kelamin: {$Dosen ['jenis_kelamin']} <br>";
+    ?>
+</body>
+</html>
