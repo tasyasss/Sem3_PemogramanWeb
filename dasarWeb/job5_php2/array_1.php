603dee686ba64b1d203873b5f74b0ec0fa648ad7
tasyasss <tasyasakina0703@gmail.com>
2024-09-25 11:01:53 +0700

    prak1

---
+++ dasarWeb/job5_php2/array_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    $ListDosen = ["Zawaruddin", "Vit Zuraida", "Annisa Taufika"];
+
+    echo $ListDosen[2]."<br>";
+    echo $ListDosen[0]."<br>";
+    echo $ListDosen[1]."<br>";
+    ?>
+</body>
+</html>
