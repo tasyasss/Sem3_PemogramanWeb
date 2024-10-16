4f4a68f042e72a35a7690cc3cd1e31b824f89bb0
tasyasss <tasyasakina0703@gmail.com>
2024-10-16 16:20:25 +0700

    job8 prak4

--- dasarWeb/job8_cookies_session/form_upload_ajax.php
+++ dasarWeb/job8_cookies_session/form_upload_ajax.php
@@ -4,18 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>prak3</title>
+
+    <link rel="stylesheet" type="text/css" href="upload.css">
 </head>
 <body>
-    <h2>Form Ajax</h2>
-    <form action="upload_ajax.php" method="post" id="upload-form" enctype="multipart/form-data">
-        <input type="file" name="file[]" id="file" multiple="multiple">
-        <br><br>
-        <input type="submit" name="submit" id="Unggah">
-    </form>
+    <div class="upload-form-container">
+        <h2>Unggah File Dokumen</h2>
+        <form id="upload-form" action="upload.php" method="POST" enctype="multipart/form-data">
+            <div class="file-input-container">
+                <input type="file" name="myFile" id="file" class="file-input">
+                <label for="file" class="file-label">Pilih File</label>
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" 
+            disabled>Unggah</button>
+        </form>
 
-    <div id="status"></div>
+        <div id="status" class="upload-status"></div>
 
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="upload.js"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="upload.js"></script>
+    </div>
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="upload.css">
+    <title>Unggah File Dokumen</title>
+</head>
