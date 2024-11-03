7f225cf1c71a6257e79ddd9c0117c80e1c11d132
tasyasss <tasyasakina0703@gmail.com>
2024-11-03 12:48:14 +0700

    job 10.2 prak5

--- dasarWeb/job10.2_crud/edit.php
+++ dasarWeb/job10.2_crud/edit.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Edit Data Anggota</title>
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -18,61 +23,40 @@
     $row = mysqli_fetch_assoc($result);
     mysqli_close($koneksi);
     ?>
-    <div class="container">
+    <div class="container mt-4">
         <h2>Edit Data Anggota</h2>
         <form action="proses.php?aksi=ubah" method="post">
             <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-            <label for="nama">Nama</label>
-            <input type="text" name="nama" id="nama" value="<?php echo $row['nama']; ?>" required>
-            <br><br>
-
-            <label>Jenis Kelamin</label>
-            <div class="radio-group">
-                <input type="radio" name="jenis_kelamin" value="L" id="laki-laki" <?php if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?> required>
-                <label for="laki-laki">Laki-laki</label>
-                <input type="radio" name="jenis_kelamin" value="P" id="perempuan" <?php if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?> required>
-                <label for="perempuan">Perempuan</label>
-            </div>
-            <br>
-
-            <label for="alamat">Alamat</label>
-            <input type="text" name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
-            <br><br>
-
-            <label for="no_telp">No. Telp</label>
-            <input type="text" name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required>
-            <br><br>
-            
-            <button type="submit" class="btn-simpan">Simpan Perubahan</button>
-            <a href="index.php" class="btn-kembali">Kembali</a>
-
-
-
-
-            <!-- <div class="form-group">
-                <label for="nama">Nama</label>
-                <input type="text" name="nama" id="nama" value="<?php echo $row['nama']; ?>" required>
-            </div>
             <div class="form-group">
-                <label>Jenis Kelamin</label>
-                <div class="radio-group">
-                    <input type="radio" name="jenis_kelamin" value="L" id="laki-laki" <?php if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?> required>
-                    <label for="laki-laki">Laki-laki</label>
-                    <input type="radio" name="jenis_kelamin" value="P" id="perempuan" <?php if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?> required>
-                    <label for="perempuan">Perempuan</label>
+                <label for="nama">Nama: </label>
+                <input type="text" class="form-control" name="nama" id="nama" value="<?php echo $row['nama']; ?>" required>
+            </div>
+
+            <div class="form-group">
+                <label for="jenis_kelamin">Jenis Kelamin</label>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="L" id="laki-laki" <?php if ($row['jenis_kelamin'] === 'L') echo 'checked'; ?> required>
+                    <label class="form-check-label" for="laki-laki">Laki-laki</label>
+                </div>
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" name="jenis_kelamin" value="P" id="perempuan" <?php if ($row['jenis_kelamin'] === 'P') echo 'checked'; ?> required>
+                    <label class="form-check-label" for="perempuan">Perempuan</label>
                 </div>
             </div>
+            
+            
             <div class="form-group">
                 <label for="alamat">Alamat</label>
-                <input type="text" name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
+                <input type="text" class="form-control" name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
             </div>
+            
             <div class="form-group">
                 <label for="no_telp">No. Telp</label>
-                <input type="text" name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required>
+                <input type="text" class="form-control" name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required>
             </div>
-            <button type="submit">Simpan Perubahan</button>
-            <a href="index.php" class="btn-kembali">Kembali</a> -->
+            <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
         </form>
+        <a href="index.php" class="btn btn-secondary mt-2">Kembali</a>
     </div>
 </body>
 
--- dasarWeb/job10.2_crud/index.php
+++ dasarWeb/job10.2_crud/index.php
@@ -6,21 +6,84 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Data Anggota</title>
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <div class="container">
+    <div class="container mt-4">
         <h2>Data Anggota</h2>
-        <a href="create.php" class="btn-tambah">Tambah Anggota</a>
-
+        <a href="create.php" class="btn btn-success mt-2">Tambah Anggota</a>
+        <br><br>
         <?php
         include('koneksi.php');
 
         $query = "SELECT * FROM anggota order by id desc";
         $result = mysqli_query($koneksi, $query);
+        ?>
 
-        if (mysqli_num_rows($result) > 0) {
+        <table class="table">
+            <thead class="thead-light">
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Alamat</th>
+                    <th>No. Telp</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                $no = 1;
+                while ($row = mysqli_fetch_array($result)) {
+                    $kelamin = ($row["jenis_kelamin"] === 'L') ? 'Laki-laki' : 'Perempuan';
+                ?>
+                <tr>
+                    <td><?=$no++?></td>
+                    <td><?=$row["nama"]?></td>
+                    <td><?=$kelamin?></td>
+                    <td><?=$row["alamat"]?></td>
+                    <td><?=$row["no_telp"]?></td>
+                    <td>
+                        <a href="edit.php?id=<?=$row["id"]?>" class="btn btn-primary">Edit</a>
+                        <a href="#" class="btn btn-danger" data-toggle='modal' data-target='#hapusModal<?=$row["id"]?>'>Hapus</a>
+                    </td>
+                </tr>
+                <div class="modal fade" id="hapusModal<?= $row["id"]?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Hapus</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                               <?= "Apakah Anda yakin ingin menghapus data dengan nama " . $row["nama"] . "?" ?> 
+                            </div>
+                            <div class="modal-footer">
+                                <a class="btn btn-danger" href="proses.php?aksi=hapus&id=<?= $row["id"] ?>">Hapus</a>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+
+
+
+
+
+        <!-- if (mysqli_num_rows($result) > 0) {
             $no = 1;
             echo "<table>";
             echo "<tr>
@@ -40,7 +103,7 @@
                     <td>" . $row["alamat"] . "</td>
                     <td>" . $row["no_telp"] . "</td>
                     <td><a href='edit.php?id=" . $row["id"] . "'>Edit</a> | 
-                    <a href='#' onclick='konfirmasiHapus(" . $row["id"] . ", \"" . $row["nama"] . "\"'>Hapus</a></td>
+                    <a href='#' onclick='konfirmasiHapus(" . $row["id"] . ", \"" . $row["nama"] . "\")'>Hapus</a></td>
                 </tr>";
             }
             echo "</table>";
@@ -48,7 +111,7 @@
             echo "Tidak ada data.";
         }
         mysqli_close($koneksi);
-        ?>
+        ?> -->
     </div>
     <script>
         function konfirmasiHapus(id, nama) {
