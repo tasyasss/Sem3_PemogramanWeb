8f95296418190ccc6a568255891aadb8ad8352fc
tasyasss <tasyasakina0703@gmail.com>
2024-11-06 14:28:57 +0700

    job 11 prak 3

---
+++ dasarWeb/job11_dinamis_struktural/admin/template/footer.php
@@ -0,0 +1,7 @@
+<script src=></script><script src="assets/dists/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.3/dist/chart.umd.js" 
+        integrity="sha384-eTlPSr3lXLlXISH3dJID1SYNJQ5xFbrkCrT7xz+ENP5MOVxBD+l05EGZioLp" 
+        crossorigin="anonymous"></script>
+
+<script src="assets/custom/dashboard.js"></script>
