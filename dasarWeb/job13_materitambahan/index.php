61e76631f38f4081055337d091b28a6b3bc41c17
tasyasss <tasyasakina0703@gmail.com>
2024-11-20 19:29:16 +0700

    job 13 prak 3

--- dasarWeb/job13_materitambahan/index.php
+++ dasarWeb/job13_materitambahan/index.php
@@ -19,8 +19,14 @@ if ($session->get('is_login') !== true) {
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="adminlte/plugins/fontawesome-free/css/all.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="adminlte/plugins/datatablesbs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="adminlte/plugins/datatablesresponsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="adminlte/plugins/datatablesbuttons/css/buttons.bootstrap4.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+  <!-- jQuery -->
+  <script src="adminlte/plugins/jquery/jquery.min.js"></script>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -82,10 +88,25 @@ if ($session->get('is_login') !== true) {
   </div>
   <!-- ./wrapper -->
 
-  <!-- jQuery -->
-  <script src="adminlte/plugins/jquery/jquery.min.js"></script>
   <!-- Bootstrap 4 -->
   <script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- jQuery Validation -->
+  <script src="adminlte/plugins/jquery-validation/jquery.validate.min.js"></script>
+  <script src="adminlte/plugins/jquery-validation/additional-methods.min.js"></script>
+  <script src="adminlte/plugins/jquery-validation/localization/messages_id.min.js"></script>
+  <!-- DataTables & Plugins -->
+  <script src="adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="adminlte/plugins/datatablesresponsive/js/dataTables.responsive.min.js"></script>
+  <script src="adminlte/plugins/datatablesresponsive/js/responsive.bootstrap4.min.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+  <script src="adminlte/plugins/jszip/jszip.min.js"></script>
+  <script src="adminlte/plugins/pdfmake/pdfmake.min.js"></script>
+  <script src="adminlte/plugins/pdfmake/vfs_fonts.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+  <script src="adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
   <!-- AdminLTE App -->
   <script src="adminlte/dist/js/adminlte.min.js"></script>
   <!-- AdminLTE for demo purposes -->
